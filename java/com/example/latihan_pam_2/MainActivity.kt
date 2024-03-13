6698e4d15cecf63d7883327b885e1efc33a768a3
121140084_Rizki Esa Fadillah <126938786+121140084-Rizki-Esa-Fadillah@users.noreply.github.com>
2024-03-13 19:41:46 +0700

    Add files via upload

---
+++ java/com/example/latihan_pam_2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.latihan_pam_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.latihan_pam_2.R.layout.activity_main
+import com.example.latihan_pam_2.ui.theme.Latihan_PAM_2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main)
+
+    }
+}
